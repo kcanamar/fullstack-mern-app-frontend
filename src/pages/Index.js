721a7f917b14ba93c14deb678c0b3d83416e03f7
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-25 20:02:44 -0700

    creates form

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,29 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
-export default function Index({ people }) {
+export default function Index({ people, createPeople }) {
+    const [newForm, setNewForm] = useState({
+        name: "",
+        image: "",
+        title: "",
+    })
+    const handleChange = (evt) => {
+        setNewForm((prevState) => (
+            {
+            ...prevState,
+            [evt.target.name]: evt.target.value,
+            }
+        ))
+    }
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        createPeople(newForm)
+        setNewForm({
+            name: "",
+            image: "",
+            title: "",
+        })
+    }
     const loaded = () => {
         return people.map((person) => (
             <div key={person._id} className="person">
@@ -16,5 +39,13 @@ export default function Index({ people }) {
     const loading = () => {
         return <h1>Loading...</h1>
     }
-    return people ? loaded() : loading()
+    return <section>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={newForm.name} name="name" placeholder="name" onChange={handleChange}/>
+                <input type="text" value={newForm.image} name="image" placeholder="image URL" onChange={handleChange}/>
+                <input type="text" value={newForm.title} name="title" placeholder="title" onChange={handleChange}/>
+                <input type="submit" value="Create Person"/>
+            </form>
+            {people ? loaded() : loading()}
+        </section>
 }
