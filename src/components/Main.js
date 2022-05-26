4447508150c3d123c0a7dc3fe85b6a7e945218db
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-25 19:19:25 -0700

    creates working api and passes api response as props to components for rendering

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,38 @@
 import { Routes, Route } from "react-router-dom"
+import { useEffect, useState } from "react"
 import Index from "../pages/Index"
 import Show from "../pages/Show"
 
 export default function Main() {
+
+    const [people, setPeople] = useState(null)
+    const URL = "https://karc-fullstack-mern-app-be.herokuapp.com/people"
+    const getPeople = async () => {
+        const response = await fetch(URL)
+        const data = await response.json()
+        setPeople(data)
+    }
+
+    const createPeople = async (person) => {
+        await fetch(URL, {
+            method: "PORT",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(person),
+        })
+        getPeople()
+    }
+
+    useEffect(() => {getPeople()}, [people])
     return (
         <main>
             <Routes>
-                <Route exact path="/" element={<Index/>}/>
-                <Route path="/people/:id" element={<Show/>}/>
+                <Route
+                    exact
+                    path="/"
+                    element={<Index people={people} createPeople={createPeople}/>}/>
+                <Route path="/people/:id" element={<Show people={people}/>}/>
             </Routes>
         </main>
     )
